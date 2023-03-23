2e240253b426e7f7d42a64588300e57b2c3b5812
Manuel Alejandro Moscoso Domínguez <manuel@mobiquos.cl>
2023-03-23 10:50:05 -0300

    Added onClickListener to the main Activity.

--- app/src/main/java/cl/mmoscoso/mygroceryinventory/MainActivity.java
+++ app/src/main/java/cl/mmoscoso/mygroceryinventory/MainActivity.java
@@ -2,15 +2,29 @@ package cl.mmoscoso.mygroceryinventory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonLogin = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonLogin = (Button) findViewById(R.id.button);
+        buttonLogin.setOnClickListener(this);
 
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                Toast.makeText(this,"Hi Hi, Login go",Toast.LENGTH_LONG).show();
+        }
     }
 }
